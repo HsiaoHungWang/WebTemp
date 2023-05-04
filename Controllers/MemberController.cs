113a98cc859b6e75f2d85cc245dccceaf348398b
hhwang <wang@ispan.com.tw>
2023-05-04 09:29:05 +0800

    Content() 方法

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebTemp.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            //html => 顯示 + 資料
+            return Content("會員首頁", "text/plain", Encoding.UTF8);
+        }
+
+        
+    }
+}
