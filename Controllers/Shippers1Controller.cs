8e97673fe3649bce4cab12eebca5eb212e794b1f
hhwang <wang@ispan.com.tw>
2023-05-04 09:00:15 +0800

    加入專案檔案。

---
+++ Controllers/Shippers1Controller.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebTemp.Models;
+
+namespace WebTemp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Shippers1Controller : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public Shippers1Controller(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Shippers1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shippers>>> GetShippers()
+        {
+          if (_context.Shippers == null)
+          {
+              return NotFound();
+          }
+            return await _context.Shippers.ToListAsync();
+        }
+
+        // GET: api/Shippers1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shippers>> GetShippers(int id)
+        {
+          if (_context.Shippers == null)
+          {
+              return NotFound();
+          }
+            var shippers = await _context.Shippers.FindAsync(id);
+
+            if (shippers == null)
+            {
+                return NotFound();
+            }
+
+            return shippers;
+        }
+
+        // PUT: api/Shippers1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShippers(int id, Shippers shippers)
+        {
+            if (id != shippers.ShipperId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(shippers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShippersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Shippers1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Shippers>> PostShippers(Shippers shippers)
+        {
+          if (_context.Shippers == null)
+          {
+              return Problem("Entity set 'NorthwindContext.Shippers'  is null.");
+          }
+            _context.Shippers.Add(shippers);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetShippers", new { id = shippers.ShipperId }, shippers);
+        }
+
+        // DELETE: api/Shippers1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShippers(int id)
+        {
+            if (_context.Shippers == null)
+            {
+                return NotFound();
+            }
+            var shippers = await _context.Shippers.FindAsync(id);
+            if (shippers == null)
+            {
+                return NotFound();
+            }
+
+            _context.Shippers.Remove(shippers);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ShippersExists(int id)
+        {
+            return (_context.Shippers?.Any(e => e.ShipperId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/ShippersController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebTemp.Models;
+
+namespace WebTemp.Controllers
+{
+    public class ShippersController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public ShippersController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        //public ShippersController()
+        //{
+        //    _context =new NorthwindContext();
+        //}
+
+        // GET: Shippers
+        public async Task<IActionResult> Index()
+        {
+              return _context.Shippers != null ? 
+                          View(await _context.Shippers.ToListAsync()) :
+                          Problem("Entity set 'NorthwindContext.Shippers'  is null.");
+        }
+
+        // GET: Shippers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Shippers == null)
+            {
+                return NotFound();
+            }
+
+            var shippers = await _context.Shippers
+                .FirstOrDefaultAsync(m => m.ShipperId == id);
+            if (shippers == null)
+            {
+                return NotFound();
+            }
+
+            return View(shippers);
+        }
+
+        // GET: Shippers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shippers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ShipperId,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shippers);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shippers);
+        }
+
+        // GET: Shippers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Shippers == null)
+            {
+                return NotFound();
+            }
+
+            var shippers = await _context.Shippers.FindAsync(id);
+            if (shippers == null)
+            {
+                return NotFound();
+            }
+            return View(shippers);
+        }
+
+        // POST: Shippers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ShipperId,CompanyName,Phone")] Shippers shippers)
+        {
+            if (id != shippers.ShipperId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shippers);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShippersExists(shippers.ShipperId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shippers);
+        }
+
+        // GET: Shippers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Shippers == null)
+            {
+                return NotFound();
+            }
+
+            var shippers = await _context.Shippers
+                .FirstOrDefaultAsync(m => m.ShipperId == id);
+            if (shippers == null)
+            {
+                return NotFound();
+            }
+
+            return View(shippers);
+        }
+
+        // POST: Shippers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Shippers == null)
+            {
+                return Problem("Entity set 'NorthwindContext.Shippers'  is null.");
+            }
+            var shippers = await _context.Shippers.FindAsync(id);
+            if (shippers != null)
+            {
+                _context.Shippers.Remove(shippers);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShippersExists(int id)
+        {
+          return (_context.Shippers?.Any(e => e.ShipperId == id)).GetValueOrDefault();
+        }
+    }
+}
