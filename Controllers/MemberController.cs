513ec4a23cff3894bbe9b406be35a84ba07fd5d4
hhwang <wang@ispan.com.tw>
2023-05-04 14:35:33 +0800

    帳號檢查

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using System.Text;
+using WebTemp.Models;
 
 namespace WebTemp.Controllers
 {
     public class MemberController : Controller
     {
+        private readonly NorthwindContext _context;
+
+        public MemberController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +24,16 @@ namespace WebTemp.Controllers
         {
             //  return Content("Hello Async Await!!", "text/plain", Encoding.UTF8);
             return Content($"Hello {name}, You are {age} years old!!", "text/plain", Encoding.UTF8);
+        }
+
+        public IActionResult Register() {
+            return View();   
+          }
+
+        public IActionResult CheckName(string name)
+        {
+            var exists = _context.Employees.Any(e => e.FirstName == name);
+            return Content(exists.ToString());
         }
 
         
